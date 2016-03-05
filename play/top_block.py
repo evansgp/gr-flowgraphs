1062f308eda8fb9c6e9167319bb8c5d70dc3e7b0
Gareth Evans <evans.g.p@gmail.com>
2016-03-05 20:29:26 +0800

    hej

---
+++ play/top_block.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Sat Mar  5 20:12:19 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import constsink_gl
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.nb = self.nb = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.nb.AddPage(grc_wxgui.Panel(self.nb), "tab1")
+        self.nb.AddPage(grc_wxgui.Panel(self.nb), "tab2")
+        self.nb.AddPage(grc_wxgui.Panel(self.nb), "tab3")
+        self.Add(self.nb)
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_f(
+        	self.nb.GetPage(1).GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="Waterfall Plot",
+        )
+        self.nb.GetPage(1).Add(self.wxgui_waterfallsink2_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.nb.GetPage(2).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.nb.GetPage(2).Add(self.wxgui_fftsink2_0.win)
+        self.wxgui_constellationsink2_0 = constsink_gl.const_sink_c(
+        	self.nb.GetPage(0).GetWin(),
+        	title="Constellation Plot",
+        	sample_rate=samp_rate,
+        	frame_rate=5,
+        	const_size=2048,
+        	M=4,
+        	theta=0,
+        	loop_bw=6.28/100.0,
+        	fmax=0.06,
+        	mu=0.5,
+        	gain_mu=0.005,
+        	symbol_rate=samp_rate/4.,
+        	omega_limit=0.005,
+        )
+        self.nb.GetPage(0).Add(self.wxgui_constellationsink2_0.win)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source("/home/gareth/tmp/SDRSharp_20160305_114954Z_27619080Hz_IQ.wav", True)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_float_to_complex_0, 0), (self.wxgui_constellationsink2_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_float_to_complex_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_waterfallsink2_0, 0))    
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_throttle_0, 0))    
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_constellationsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
